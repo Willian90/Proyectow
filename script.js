e1b2bae53ea22d63807025ea8e1d3a36c40a6808
Willian Espinoza <willian.adalberto.espinoza@gmail.com>
2020-05-02 17:39:19 -0600

    Poniendo el script en un solo archivo

---
+++ script.js
@@ -0,0 +1,17 @@
+const vue = new Vue({
+    el: "#app",
+    data: {
+        message: "Mi Primer Aplicacion Con Vue JS",
+        alerta: "alert alert-primary",
+        centrar: "text-center",
+        frutas: [
+            {nombre:"melón", cantidad:4},
+            {nombre:"Papaya", cantidad:9},
+            {nombre:"Banana", cantidad:7},
+            {nombre:"Sandía", cantidad:0},
+            {nombre:"Guayaba", cantidad:23}
+            
+            ]
+        
+    }
+})
